d4fab6af9fd6d1bd89e2b4816d527b0661131637
Black Shark <luong.chinh.21.11@gmail.com>
2019-07-13 11:44:27 +0700

    ad file: ex_JavaScript

---
+++ ex_JavaScript/JavaScript_Basic/1_current_day_&&_time.js
@@ -0,0 +1,45 @@
+/*
+Write a JavaScript program to display the current day and time in the following format.
+Today is : Tuesday. 
+Current time is : 10 PM : 30 : 38
+ */
+
+const today = new Date();
+const day = today.getDay();
+const dayList = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+console.log('Today is: ' + dayList[day]);
+
+let hours = today.getHours();
+const prepand = (hours >= 12) ? ' PM ' : ' AM ';
+hours = (hours >= 12) ? hours - 12 : hours;
+const minutes = today.getMinutes();
+const seconds = today.getSeconds();
+
+if (hours === 0 && prepand === ' PM ') {
+    if (minutes === 0 && seconds === 0) {
+        hours = 12;
+        prepand = ' Noon ';
+    } else {
+        hours = 12;
+        prepand = ' AM ';
+    }
+} else if (hours === 0 && prepand === ' AM ') {
+    if (minutes === 0 && seconds === 0) {
+        hours = 12;
+        prepand = ' Midnight ';
+    } else {
+        hours = 12;
+        prepand = ' PM ';
+    }
+} 
+
+console.log(`Current time is: ${hours}${prepand} : ${minutes} : ${seconds}`);
