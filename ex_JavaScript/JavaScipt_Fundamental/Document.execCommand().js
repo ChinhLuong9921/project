1a89fcb20533dbe9502e089730b83dd8eef73493
Black Shark <luong.chinh.21.11@gmail.com>
2019-08-02 01:20:26 +0700

    update JS Fundamental (1)

---
+++ ex_JavaScript/JavaScipt_Fundamental/Document.execCommand().js
@@ -0,0 +1,132 @@
+const copy_to_Clipboard = str => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    // <textarea readonly="" style="position: absolute; left: -9999px;"></textarea>
+  
+    const selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+    // getSelection().getRangeAt(0) : lấy range từ đoạn text được bôi đen
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    if (selected) {
+      document.getSelection().removeAllRanges();
+      // xóa range
+      document.getSelection().addRange(selected);
+    }
+};
+
+// Paragraph
+
+document.addEventListener('DOMContentLoaded', () => {
+    const p = document.querySelector.bind(document);
+    const bold_btn = p('#bold-btn'),
+          italic_btn = p('#italic-btn'),
+          underline_btn = p('#underline-btn'),
+          strike_through_btn = p('#strike-through-btn'),
+          subscript_btn = p('#subscript-btn'),
+          superscript_btn = p('#superscript-btn'),
+          color_btn = p('#color-btn'),
+          size_btn = p('#size-btn'),
+          delete_btn = p('#delete-btn'),
+          undo_btn = p('#undo-btn'),
+          redo_btn = p('#redo-btn');
+    bold_btn.addEventListener('click', () => {
+        document.execCommand('bold');
+    });
+    italic_btn.addEventListener('click', () => {
+        document.execCommand('italic');
+    });
+    underline_btn.addEventListener('click', () => {
+        document.execCommand('underline');
+    });
+    strike_through_btn.addEventListener('click', () => {
+        document.execCommand('strikeThrough');
+    });
+    subscript_btn.addEventListener('click', () => {
+        document.execCommand('subscript');
+    });
+    superscript_btn.addEventListener('click', () => {
+        document.execCommand('superscript');
+    });
+    color_btn.addEventListener('click', () => {
+        document.execCommand('styleWithCSS', false, true);
+        document.execCommand('foreColor', false, 'red');
+    });
+    size_btn.addEventListener('click', () => {
+        document.execCommand('styleWithCSS', false, true);
+        document.execCommand('fontSize', false, '30px');
+    });
+    delete_btn.addEventListener('click', () => {
+        document.execCommand('delete');
+    });
+    undo_btn.addEventListener('click', () => {
+        document.execCommand('undo');
+    });
+    redo_btn.addEventListener('click', () => {
+        document.execCommand('redo');
+    });
+});
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     const $ = document.querySelector.bind(document);
+//     const boldBtn = $("#bold-btn"),
+//           italicBtn = $("#italic-btn"),
+//           underlineBtn = $("#underline-btn"),
+//           strikeThroughBtn = $("#strike-through-btn"),
+//           subscriptBtn = $("#supscript-btn"),
+//           superscriptBtn = $("#superscript-btn"),
+//           colorBtn = $("#color-btn"),
+//           sizeBtn = $("#size-btn"),
+//           deleteBtn = $("#delete-btn"),
+//           undoBtn = $("#undo-btn"),
+//           redoBtn = $("#redo-btn");
+    
+//     boldBtn.addEventListener("click", () => {
+//       document.execCommand('bold');
+//     });
+    
+//     italicBtn.addEventListener("click", () => {
+//       document.execCommand('italic');
+//     });
+    
+//     underlineBtn.addEventListener("click", () => {
+//       document.execCommand('underline');
+//     });
+    
+//     strikeThroughBtn.addEventListener("click", () => {
+//       document.execCommand('strikeThrough');
+//     });
+    
+//     subscriptBtn.addEventListener("click", () => {
+//       document.execCommand('subscript');
+//     });
+    
+//     superscriptBtn.addEventListener("click", () => {
+//       document.execCommand('superscript');
+//     });
+    
+//     colorBtn.addEventListener("click", () => {
+//       document.execCommand('styleWithCSS', false, true);
+//       document.execCommand('foreColor', false, 'hotpink');
+//     });
+// sizeBtn.addEventListener("click", () => {
+//     document.execCommand('styleWithCSS', false, true);
+//     document.execCommand('fontSize', false, '25px');
+//   });
+  
+//   deleteBtn.addEventListener("click", () => {
+//     document.execCommand('delete');
+//   });
+  
+//   undoBtn.addEventListener("click", () => {
+//     document.execCommand('undo');
+//   });
+  
+//   redoBtn.addEventListener("click", () => {
+//     document.execCommand('redo');
+//   });
+// });
---
+++ ex_JavaScript/JavaScipt_Fundamental/_test.js
@@ -0,0 +1,20 @@
+const copy_to_Clipboard = str => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  // <textarea readonly="" style="position: absolute; left: -9999px;"></textarea>
+
+  const selected = document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+  // getSelection().getRangeAt(0) : lấy range từ đoạn text được bôi đen
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  if (selected) {
+    document.getSelection().removeAllRanges();
+    // xóa range
+    document.getSelection().addRange(selected);
+  }
+};
